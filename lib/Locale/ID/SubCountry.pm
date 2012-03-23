a4a2b9a52e364157ebca8f014bf38093fb11c000
Steven Haryanto <stevenharyanto@gmail.com>
2012-03-23 07:25:36 +0000

    Import of SHARYANTO/Locale-ID-Province-0.02 from CPAN.
    gitpan-cpan-distribution: Locale-ID-Province
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Locale-ID-Province-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Locale/ID/SubCountry.pm
+++ lib/Locale/ID/SubCountry.pm
@@ -1,5 +1,8 @@
 package Locale::ID::SubCountry;
 use Locale::ID::Province;
+
+our $VERSION = '0.02'; # VERSION
+
 our @ISA       = @Locale::ID::Province::ISA;
 our @EXPORT    = @Locale::ID::Province::EXPORT;
 our @EXPORT_OK = @Locale::ID::Province::EXPORT_OK;
@@ -20,7 +23,281 @@ Locale::ID::SubCountry - Alias for Locale::ID::Province
 
 =head1 VERSION
 
-version 0.01
+version 0.02
+
+=head1 FUNCTIONS
+
+
+=head2 list_id_provinces(%args) -> [status, msg, result, meta]
+
+REPLACE ME.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<bps_code>* => I<int>
+
+Only return records where the 'bps_code' field equals specified value.
+
+=item * B<bps_code.is>* => I<int>
+
+Only return records where the 'bps_code' field equals specified value.
+
+=item * B<bps_code.max> => I<int>
+
+Only return records where the 'bps_code' field is less than or equal to specified value.
+
+=item * B<bps_code.min> => I<array>
+
+Only return records where the 'bps_code' field is greater than or equal to specified value.
+
+=item * B<bps_code.xmax> => I<int>
+
+Only return records where the 'bps_code' field is less than specified value.
+
+=item * B<bps_code.xmin> => I<array>
+
+Only return records where the 'bps_code' field is greater than specified value.
+
+=item * B<detail> => I<bool> (default: 0)
+
+Return array of full records instead of just ID fields.
+
+By default, only the key (ID) field is returned per result entry.
+
+=item * B<eng_name>* => I<str>
+
+Only return records where the 'eng_name' field equals specified value.
+
+=item * B<eng_name.contains> => I<str>
+
+Only return records where the 'eng_name' field contains specified text.
+
+=item * B<eng_name.is>* => I<str>
+
+Only return records where the 'eng_name' field equals specified value.
+
+=item * B<eng_name.max> => I<str>
+
+Only return records where the 'eng_name' field is less than or equal to specified value.
+
+=item * B<eng_name.min> => I<array>
+
+Only return records where the 'eng_name' field is greater than or equal to specified value.
+
+=item * B<eng_name.not_contains> => I<str>
+
+Only return records where the 'eng_name' field does not contain a certain text.
+
+=item * B<eng_name.xmax> => I<str>
+
+Only return records where the 'eng_name' field is less than specified value.
+
+=item * B<eng_name.xmin> => I<array>
+
+Only return records where the 'eng_name' field is greater than specified value.
+
+=item * B<fields>* => I<array>
+
+Select fields to return.
+
+=item * B<ind_capital_name>* => I<str>
+
+Only return records where the 'ind_capital_name' field equals specified value.
+
+=item * B<ind_capital_name.contains> => I<str>
+
+Only return records where the 'ind_capital_name' field contains specified text.
+
+=item * B<ind_capital_name.is>* => I<str>
+
+Only return records where the 'ind_capital_name' field equals specified value.
+
+=item * B<ind_capital_name.max> => I<str>
+
+Only return records where the 'ind_capital_name' field is less than or equal to specified value.
+
+=item * B<ind_capital_name.min> => I<array>
+
+Only return records where the 'ind_capital_name' field is greater than or equal to specified value.
+
+=item * B<ind_capital_name.not_contains> => I<str>
+
+Only return records where the 'ind_capital_name' field does not contain a certain text.
+
+=item * B<ind_capital_name.xmax> => I<str>
+
+Only return records where the 'ind_capital_name' field is less than specified value.
+
+=item * B<ind_capital_name.xmin> => I<array>
+
+Only return records where the 'ind_capital_name' field is greater than specified value.
+
+=item * B<ind_island_name>* => I<str>
+
+Only return records where the 'ind_island_name' field equals specified value.
+
+=item * B<ind_island_name.contains> => I<str>
+
+Only return records where the 'ind_island_name' field contains specified text.
+
+=item * B<ind_island_name.is>* => I<str>
+
+Only return records where the 'ind_island_name' field equals specified value.
+
+=item * B<ind_island_name.max> => I<str>
+
+Only return records where the 'ind_island_name' field is less than or equal to specified value.
+
+=item * B<ind_island_name.min> => I<array>
+
+Only return records where the 'ind_island_name' field is greater than or equal to specified value.
+
+=item * B<ind_island_name.not_contains> => I<str>
+
+Only return records where the 'ind_island_name' field does not contain a certain text.
+
+=item * B<ind_island_name.xmax> => I<str>
+
+Only return records where the 'ind_island_name' field is less than specified value.
+
+=item * B<ind_island_name.xmin> => I<array>
+
+Only return records where the 'ind_island_name' field is greater than specified value.
+
+=item * B<ind_name>* => I<str>
+
+Only return records where the 'ind_name' field equals specified value.
+
+=item * B<ind_name.contains> => I<str>
+
+Only return records where the 'ind_name' field contains specified text.
+
+=item * B<ind_name.is>* => I<str>
+
+Only return records where the 'ind_name' field equals specified value.
+
+=item * B<ind_name.max> => I<str>
+
+Only return records where the 'ind_name' field is less than or equal to specified value.
+
+=item * B<ind_name.min> => I<array>
+
+Only return records where the 'ind_name' field is greater than or equal to specified value.
+
+=item * B<ind_name.not_contains> => I<str>
+
+Only return records where the 'ind_name' field does not contain a certain text.
+
+=item * B<ind_name.xmax> => I<str>
+
+Only return records where the 'ind_name' field is less than specified value.
+
+=item * B<ind_name.xmin> => I<array>
+
+Only return records where the 'ind_name' field is greater than specified value.
+
+=item * B<iso3166_2_code>* => I<str>
+
+Only return records where the 'iso3166_2_code' field equals specified value.
+
+=item * B<iso3166_2_code.contains> => I<str>
+
+Only return records where the 'iso3166_2_code' field contains specified text.
+
+=item * B<iso3166_2_code.is>* => I<str>
+
+Only return records where the 'iso3166_2_code' field equals specified value.
+
+=item * B<iso3166_2_code.max> => I<str>
+
+Only return records where the 'iso3166_2_code' field is less than or equal to specified value.
+
+=item * B<iso3166_2_code.min> => I<array>
+
+Only return records where the 'iso3166_2_code' field is greater than or equal to specified value.
+
+=item * B<iso3166_2_code.not_contains> => I<str>
+
+Only return records where the 'iso3166_2_code' field does not contain a certain text.
+
+=item * B<iso3166_2_code.xmax> => I<str>
+
+Only return records where the 'iso3166_2_code' field is less than specified value.
+
+=item * B<iso3166_2_code.xmin> => I<array>
+
+Only return records where the 'iso3166_2_code' field is greater than specified value.
+
+=item * B<q> => I<str>
+
+Search.
+
+=item * B<random> => I<bool> (default: 0)
+
+Return records in random order.
+
+=item * B<result_limit> => I<int>
+
+Only return a certain number of records.
+
+=item * B<result_start> => I<int> (default: 1)
+
+Only return starting from the n'th record.
+
+=item * B<sort> => I<str>
+
+Order records according to certain field(s).
+
+A list of field names separated by comma. Each field can be prefixed with '-' to
+specify descending order instead of the default ascending.
+
+=item * B<tags>* => I<str>
+
+Only return records where the 'tags' field equals specified value.
+
+=item * B<tags.contains> => I<str>
+
+Only return records where the 'tags' field contains specified text.
+
+=item * B<tags.is>* => I<str>
+
+Only return records where the 'tags' field equals specified value.
+
+=item * B<tags.max> => I<str>
+
+Only return records where the 'tags' field is less than or equal to specified value.
+
+=item * B<tags.min> => I<array>
+
+Only return records where the 'tags' field is greater than or equal to specified value.
+
+=item * B<tags.not_contains> => I<str>
+
+Only return records where the 'tags' field does not contain a certain text.
+
+=item * B<tags.xmax> => I<str>
+
+Only return records where the 'tags' field is less than specified value.
+
+=item * B<tags.xmin> => I<array>
+
+Only return records where the 'tags' field is greater than specified value.
+
+=item * B<with_field_names> => I<bool>
+
+Return field names in each record (as hash/associative array).
+
+When enabled, function will return each record as hash/associative array
+(field name => value pairs). Otherwise, function will return each record
+as list/array (field value, field value, ...).
+
+=back
+
+Return value:
+
+Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
 
 =head1 AUTHOR
 
@@ -28,7 +305,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
